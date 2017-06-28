b7294e797f6d1e76684409b54c513f1f64a3fb52
Jose Saldana <jmsaldana@gmail.com>
2017-06-28 11:30:31 -0500

    002 - Added screen utility class

--- siva-utp/Screen.h
+++ siva-utp/Screen.h
@@ -1,10 +1,28 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-class Screen {
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+const int CONTRAST_1        = 60;
+const int CONTRAST_2        = 0; // CREANDO VARIABLE DE CONTRASTE
+
+class ScreenUtils {
   public:
-    static void print(char* text) {
-      Serial.write(text);
+    static configDisplay(Adafruit_PCD8544 display) {
+      pinMode(7,OUTPUT);
+      
+      digitalWrite(7,LOW); // PUERTO 7 ES EL DE LA LUZ SE PUEDE ENCENDER Y APAGAR (LOW, HIGH)
+
+      // Display for Voting
+      display.begin(); //INICIAR PANTALLA     
+      display.setContrast(CONTRAST_1); // COLOCAR VALOR AL CONTRASTE
+      display.clearDisplay(); // LIMPIAR PANTALLA
+      display.display();   //DESPLEGAR PANTALLA (ACTUALIZAR)
+    }
+
+    static void displayText(char* text, Adafruit_PCD8544 display) { 
+      display.clearDisplay();  
     }
 };
 
