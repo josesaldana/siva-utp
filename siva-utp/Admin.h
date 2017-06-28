7a0f36120edd113894a62f976076b008bbd155e2
Jose Saldana <jmsaldana@gmail.com>
2017-06-28 11:43:35 -0500

    006 - Initial structure of voting terminal setup handler

--- siva-utp/Admin.h
+++ siva-utp/Admin.h
@@ -2,12 +2,27 @@
 #define ADMIN_H
 
 #include "Arduino.h"
-#include "Print.h"
+#include "Printer.h"
+#include "Database.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h> // LIBRERIAS NECESARIAS
+
+Adafruit_PCD8544 DISPLAY_A = Adafruit_PCD8544( 5, 4, 3);
 
 class Admin {
   public:
     Admin();
+
+    /**
+     * Main run code.  This will take care of 
+     * session management sub-state machine.
+     */
     void run();
 };
+
+Admin::Admin() {}
+
+void Admin::run() {
+}
 
 #endif //ADMIN_H
